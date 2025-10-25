d004febcf6af5009b8c49cd830af0a9b939b8908
javilujann <jp.lujanromero@um.es>
2025-10-25 10:35:34 +0200

    Diagnostico: generate getters and setters

--- src/main/java/diagnosticos/Diagnostico.java
+++ src/main/java/diagnosticos/Diagnostico.java
@@ -9,7 +9,7 @@ public class Diagnostico {
 	private TipoDiagnostico tipo;
 	private Severidad severidad;
 	
-	
+
 	public Diagnostico(float confianza, PatronPatologico evidencia, TipoDiagnostico tipo, Severidad severidad) {
 		super();
 		this.confianza = confianza;
@@ -18,6 +18,36 @@ public class Diagnostico {
 		this.severidad = severidad;
 	}
 
+	
+	
+	public float getConfianza() {
+		return confianza;
+	}
+
+
+	public void setConfianza(float confianza) {
+		this.confianza = confianza;
+	}
+
+
+	public PatronPatologico getEvidencia() {
+		return evidencia;
+	}
+
+
+	public void setEvidencia(PatronPatologico evidencia) {
+		this.evidencia = evidencia;
+	}
+
+
+	public Severidad getSeveridad() {
+		return severidad;
+	}
+
+
+	public void setSeveridad(Severidad severidad) {
+		this.severidad = severidad;
+	}
 
 	public TipoDiagnostico getTipo() {
 		return tipo;
