7b2cebdde5472049bd27ee4ec6a0b4e8ea88cb00
javilujann <jp.lujanromero@um.es>
2025-11-12 20:29:26 +0100

    Añadido puntero de Onda a Ciclo

--- src/main/java/componentes/Onda.java
+++ src/main/java/componentes/Onda.java
@@ -1,13 +1,13 @@
 package componentes;
 
+import elem.Ciclo;
 
 public class Onda extends Componente {
 	
 	private OndaTipo tipo;
 	private float amplitud;
+	private Ciclo ciclo;
 	
-	
-
 	public Onda(float amplitud, float inicio, float fin, OndaTipo tipo) {
 		super(inicio, fin);
 		this.amplitud = amplitud;
@@ -28,6 +28,14 @@ public class Onda extends Componente {
 
 	public void setAmplitud(float amplitud) {
 		this.amplitud = amplitud;
+	}
+
+	public Ciclo getCiclo() {
+		return ciclo;
+	}
+
+	public void setCiclo(Ciclo ciclo) {
+		this.ciclo = ciclo;
 	}
 	
 }
