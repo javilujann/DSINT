4458120db4b3fae7e1a5a4f1314c63032a8fa05d
javilujann <jp.lujanromero@um.es>
2025-11-18 21:03:48 +0100

    Typo

---
+++ src/main/java/diagnosticos/IsquemiaCoronaria.java
@@ -0,0 +1,11 @@
+package diagnosticos;
+
+import patrones.PatronPatologico;
+
+public class IsquemiaCoronaria extends Diagnostico {
+
+	public IsquemiaCoronaria(float confianza, PatronPatologico evidencia) {
+		super(confianza, evidencia);
+	}
+
+}
